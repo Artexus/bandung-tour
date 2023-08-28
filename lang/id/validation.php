628faf2ebb1a3c2e9a1489e4d4460ed1918a92db
Artexus <matthewsantoso53@gmail.com>
2023-08-28 22:56:46 +0700

    feat: add id locale for validator

---
+++ lang/id/validation.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'email' => 'Kolom :attribute harus alamat e-mail yang valid.',
+    'required' => 'Kolom :attribute harus diisi.',
+    'min' => [
+        'array' => ':attribute harus memiliki panjang setidaknya :min.',
+        'file' => ':attribute harus memiliki size minimal :min kilobytes.',
+        'numeric' => ':attribute harus memiliki minimal :min.',
+        'string' => ':attribute harus memiliki setidaknya :min karakter.',
+    ],
+    'same' => ':attribute dan :other harus sama.',
+    'regex' => 'format pada field :attribute tidak valid.'
+];
